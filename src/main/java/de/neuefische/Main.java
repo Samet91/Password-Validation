db9005a981c8b81788983f5192bc6f055d8a42b4
Samet Uslu <samet_u@hotmail.de>
2022-02-05 12:16:32 +0100

    checkArray

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -3,8 +3,13 @@ package de.neuefische;
 public class Main {
 
     public static void main(String[] args) {
+        String[] passwordArray = new String[3];
+        passwordArray[0] = "HelloWorld1";
+        passwordArray[1] = "HelloCoaches1";
+        passwordArray[2] = "HelloStudents1";
 
-        System.out.println(Validator.createPassword("hausgarten12"));
+        System.out.println(Validator.checkPasswordsHasConditionalsFromValidateMethod(passwordArray));
+        System.out.println(Validator.createPassword("Hausgarten12"));
     }
 
 }
